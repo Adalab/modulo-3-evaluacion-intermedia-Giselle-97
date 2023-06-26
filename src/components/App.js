f42ebab7fb9fa8573001f6f94461bedbbef5cc3e
Giselle-97 <giselle.cduran@hotmail.com>
2023-06-26 11:07:59 +0200

    added filtered

--- src/components/App.js
+++ src/components/App.js
@@ -27,20 +27,67 @@ function App() {
       </li>
     ));
   };
+  //filtro
+  // Función para filtrar citas
+const handleQuoteFilter = (event) => {
+    const { value } = event.target;
+setFilteredQuotes(value);
+};};
+
+// Función para filtrar personajes
+  
+const handleCharacterFilter = (event) => {
+    const { value } = event.target;
+    setFilterCharacter(value);
+  };
+
+// Función para renderizar la lista filtrada
+const renderList = () => {
+let filteredList = list;
+if (filteredQuotes) {
+      filteredList = filteredList. 
+filter((elementList) =>
+        elementList.    
+quote.includes(filteredQuotes)
+      );
+}
+    if (filterCharacter) {
+      filteredList = filteredList.
+      filteredList
+filter((elementList) =>
+        elementList.character.includes(filterCharacter)
+      );
+    }
+      );
+    }
+return filteredList.map((elementList, i) => (
+      
+     
+<li key={i}>
+        {elementList.quote} {elementList.character}
+      </li>
+    ));
+  };
+
 
   return (
     //CÓDIGO HTML
-    <div>
+   <div>
       <header>
         <h1>Frases de Friends</h1>
       </header>
-      <form action=''>
+      <form>
         <p>Filtrar por frase</p>
-        <input type='text' />
+        <input type='text' onChange={handleQuoteFilter} />
       </form>
       <p>Filtrar por personaje</p>
-      <select name='select' id='select'>
-        <option value='Todos' disabled></option>
+      <select name='select' id='select' onChange={handleCharacterFilter}>
+        <option value='' disabled></option>
+        {list.map((elementList, i) => (
+          <option key={i} value={elementList.character}>
+            {elementList.character}
+          </option>
+        ))}
       </select>
 
       <ul>{renderList()}</ul>
