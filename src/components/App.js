cbbc55374d8d036f5d1a2e8b864a181ff8f8ce37
Giselle-97 <giselle.cduran@hotmail.com>
2023-06-26 18:13:36 +0200

    modified the evaluation, adding the parts that were missing

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
-//sección import
-//React, archivos propios, Sass, Images
-import '../styles/App.css';
 import { useEffect, useState } from 'react';
-//Componente
+import '../styles/App.css';
+//Component
 function App() {
   const [list, setList] = useState([]);
-  const [filteredQuotes, setFilteredQuotes] = useState('');
-  const [filterCharacter, setFilterCharacter] = useState('');
+  const [filteredQuote, setFilteredQuote] = useState('');
+  const [filterCharacter, setFilterCharacter] = useState('all');
+  const [newQuote, setNewQuote] = useState({});
 
-  //Fetch
+  //1-Fetch
   useEffect(() => {
     fetch(
       'https://beta.adalab.es/curso-intensivo-fullstack-recursos/apis/quotes-friends-tv-v1/quotes.json'
@@ -19,74 +18,82 @@ function App() {
       });
   }, []);
 
-  //Function and Event
+  //3-Function change the content of the state variable of quote
+  const handleQuote = (ev) => {
+    setFilteredQuote(ev.target.value);
+  };
+
+  //4-Function change the content of the state variable of character
+  const handleCharacter = (ev) => {
+    setFilterCharacter(ev.target.value);
+  };
+
+  //5-Function handleNewQuote
+  const handleNewQuote = (ev) => {
+    setNewQuote({
+      ...newQuote,
+      [ev.target.name]: ev.target.value,
+    });
+  };
+
+  //6-Function handleClick
+  const handleClick = (ev) => {
+    ev.preventDefault();
+    setList([...list, newQuote]);
+    setNewQuote({
+      quote: '',
+      character: '',
+    });
+  ;
+
+  //2-Function renderList and filter
   const renderList = () => {
-    return list.map((elementList, i) => (
-      <li key={i}>
-        {elementList.quote} {elementList.character}
-      </li>
-    ));
+    return list
+      .filter((elementList) =>
+        elementList.quote.toLowerCase().includes(filteredQuote.toLowerCase())
+      )
+      .filter((elementList) => {
+        if (filterCharacter === 'all') {
+          return elementList;
+        } else {
+          return elementList.character === filterCharacter;
+        }
+      })
+      .map((elementList, i) => (
+        <li key={i}>
+          <h4> {elementList.quote} </h4>
+          <span>{elementList.character}</span>
+        </li>
+      ));
   };
-  //filtro
-  // Función para filtrar citas
-  const handleQuoteFilter = (event) => {
-    const { value } = event.target;
-    setFilteredQuotes(value);
-  };
-
-  // Función para filtrar personajes
-
-  const handleCharacterFilter = (event) => {
-    const { value } = event.target;
-    setFilterCharacter(value);
-  };
-
-  // Función para renderizar la lista filtrada
-  /*const renderList = () => {
-    let filteredList = list;
-    if (filteredQuotes) {
-      filteredList = filteredList.filter((elementList) =>
-        elementList.quote.includes(filteredQuotes)
-      );
-    }
-    if (filterCharacter) {
-      filteredList = filteredList.filteredList;
-      filter((elementList) => elementList.character.includes(filterCharacter));
-    }
-    return filteredList.map((elementList, i) => (
-      <li key={i}>
-        {elementList.quote} {elementList.character}
-      </li>
-    ));
-  };*/
 
   return (
-    //CÓDIGO HTML
     <div>
       <header>
         <h1>Frases de Friends</h1>
       </header>
       <form>
-        <p>Filtrar por frase</p>
-        <input type='text' onChange={handleQuoteFilter} />
+        <label>Filtrar por personaje</label>
+        <input type='text' onInput={handleQuote} value={filteredQuote} />
+        <label>Filtrar por personaje</label>
+        <select name='' id='' onChange={handleCharacter}>
+          <option value='all'>Todos</option>
+          <option value='Joey'>Joey</option>
+          <option value='Phoebe'>Phoebe</option>
+          <option value='Chandler'>Chandler</option>
+          <option value='Rachel'>Rachel</option>
+          <option value='Ross'>Ross</option>
+          <option value='Monica'>Monica</option>
+        </select>
       </form>
-      <p>Filtrar por personaje</p>
-      <select name='select' id='select' onChange={handleCharacterFilter}>
-        <option value='' disabled></option>
-        {list.map((elementList, i) => (
-          <option key={i} value={elementList.character}>
-            {elementList.character}
-          </option>
-        ))}
-      </select>
-
       <ul>{renderList()}</ul>
-
-      <h1>Añadir una nueva frase</h1>
-      <p>Frase</p>
-      <input type='text' />
-      <p>Personaje</p>
-      <input type='text' />
+      <form action=''>
+        <label>Frase</label>
+        <input type='text' name='quote' onInput={handleNewQuote} value={newQuote.quote}/>
+        <label>Personaje</label>
+        <input type='text' name='character' onInput={handleNewQuote} value={newQuote.character} />
+        <button onClick={handleClick}>Añadir la nueva frase</button>
+      </form>
     </div>
   );
 }
